e126f70f814163435838ea33259e4da1276e15e2
Hamzah Hafejee <sc19mhh@leeds.ac.uk>
2022-11-17 20:07:40 +0000

    added prepopulating records

--- hamzah-API/app/db_create.py
+++ hamzah-API/app/db_create.py
@@ -1,5 +1,8 @@
 from config import SQLALCHEMY_DATABASE_URI
-from app import db
+from app import db, models
 import os.path
 
 db.create_all() #creates the db tables
+object = models.League(league_name="Premier League")
+db.session.add(object)
+db.session.commit()
--- hamzah-API/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField
-from wtforms.validators import *
-
-class League(Form): #defining user class
-    league_name = TextField('league_name', validators=[DataRequired()]) #validates that the form isnt empty
-
