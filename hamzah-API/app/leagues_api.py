d028105e4e67475812bdc8189f56f65907f73c7e
Hamzah Hafejee <sc19mhh@leeds.ac.uk>
2022-11-17 22:52:16 +0000

    apis

--- hamzah-API/app/leagues_api.py
+++ hamzah-API/app/leagues_api.py
@@ -6,21 +6,42 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-class League(Resource):
+class GetLeague(Resource):
 
     def get(self, country):
-        league = League.Property.query.filter_by(league_name = country).first()
+        league = models.League.Property.query.filter_by(country = country).first()
         dict = {'country' : country,
                 'league' : league}
-        league = json.dump(dict)
+        league = json.dumps(dict)
+        print(f"returned {league}")
         return league
 
+class PostLeague(Resource):
+
     def post(self, league, country):
-        
-        return json
+        item = models.League(league_name="Premier League", country="England")
+        db.session.add(item)
+        db.session.commit()
+        posted = {'country' : country,
+                'league' : league}
+        posted = json.dumps(posted)
+        print(f"posted '{posted}' to the db")
+        return posted
 
-    def put(self, league):
-        return json
 
+class PutLeague(Resource):
+    def put(self, old_league, new_league):
+        league = models.League.Property.query.filter_by(league_name = old_league).first()
+        league.league_name = new_league
+        db.session.commit()
+        print (f"changed '{old_league}' to '{new_league}'")
+        changed = {'old_league':old_league,
+                    'new_league':new_league}
+        changed = json.dumps(changed)
+        return changed
+
+class DeleteLeague(Resource):
     def delete(self, league=None, id=None):
         return json
+
+api.add_resource(League, )
