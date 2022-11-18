f3f1959f968dd0e5f87f00874248fbb4e52604ac
Hamzah Hafejee <sc19mhh@leeds.ac.uk>
2022-11-18 19:55:33 +0000

    home page and upd reqs.txt

--- client/app/forms.py
+++ client/app/forms.py
@@ -2,5 +2,5 @@ from flask_wtf import Form
 from wtforms import TextField
 from wtforms.validators import *
 
-class Country(Form): #defining user class
+class CountryForm(Form): #defining user class
     country = TextField('country')
