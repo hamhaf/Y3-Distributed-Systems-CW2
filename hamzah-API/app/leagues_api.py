9fb6690ca4bfb3217df08ea2f5adf9c06985d179
Hamzah Hafejee <sc19mhh@leeds.ac.uk>
2022-11-18 01:17:27 +0000

    added parser

--- hamzah-API/app/leagues_api.py
+++ hamzah-API/app/leagues_api.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from app import models, db, app
 
+parser = reqparse.RequestParser()
 
 api = Api(app)
 
 class GetLeague(Resource):
 
-    def get(self):
-        print("here")
-        country = "England"
+    def get(self, country):
         league = models.League.query.filter_by(country = country).first().league_name
         dict = {'country' : country,
                 'league' : league}
@@ -19,13 +18,18 @@ class GetLeague(Resource):
 
 class PostLeague(Resource):
 
-    def post(self, league, country):
+    def __init__(self):
+        self.league = parser.parse_args().get('league',None)
+        self.country = parser.parse_args().get('country',None)
+        
+
+    def post(self):
         print("here")
-        item = models.League(league_name=league, country=country)
+        item = models.League(league_name=self.league, country=self.country)
         db.session.add(item)
         db.session.commit()
-        posted = {'country' : country,
-                'league' : league}
+        posted = {'country' : self.country,
+                'league' : self.league}
         print(f"posted '{posted}' to the db")
         
         return posted
@@ -34,7 +38,6 @@ class PostLeague(Resource):
 class PutLeague(Resource):
 
     def put(self, old_league, new_league):
-        print("here")
         league = models.League.query.filter_by(league_name = old_league).first()
         league.league_name = new_league
         db.session.commit()
@@ -49,14 +52,13 @@ class PutLeague(Resource):
 class DeleteLeague(Resource):
 
     def delete(self, id):
-        print("here")
         models.League.query.filter_by(id = id).delete()
         deleted = {'deleted':id}
         print(f"deleted record with id: {id}")
         print(deleted)
         return deleted
 
-api.add_resource(GetLeague, '/league/getleague/')
-api.add_resource(PostLeague, '/league/postleague/<string:league>&<string:country>')
+api.add_resource(GetLeague, '/league/getleague/<string:country>')
+api.add_resource(PostLeague, '/league/postleague/')
 api.add_resource(PutLeague, '/league/putleague/<string:old_league>&<string:new_league>')
 api.add_resource(DeleteLeague, '/league/deleteleague/<int:id>')
