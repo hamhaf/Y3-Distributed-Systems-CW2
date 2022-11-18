6ce428f9d4fdb652ea2104430bf93e55d6b26eb6
Hamzah Hafejee <sc19mhh@leeds.ac.uk>
2022-11-18 19:31:40 +0000

    changed forms py

--- client/app/forms.py
+++ client/app/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import Form
-from wtforms import DateField, TextField, BooleanField, FileField, PasswordField
+from wtforms import TextField
 from wtforms.validators import *
 
-class UserForm(Form): #defining user class
-    username = TextField('username', validators=[DataRequired()]) #validates that the form isnt empty
-    password = PasswordField('password', validators=[DataRequired()]) #validates that the form isnt empty
+class Country(Form): #defining user class
+    country = TextField('country')
