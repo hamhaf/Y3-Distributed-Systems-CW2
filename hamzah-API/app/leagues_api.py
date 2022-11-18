51e491bc97fb2ad7e6c8f8a0b9f447407b3f2cdd
Hamzah Hafejee <sc19mhh@leeds.ac.uk>
2022-11-18 00:57:09 +0000

    FINALLY WORKS

--- hamzah-API/app/leagues_api.py
+++ hamzah-API/app/leagues_api.py
@@ -1,35 +1,32 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
-from app import models, db
-import json
+from app import models, db, app
 
-app = Flask(__name__)
+
 api = Api(app)
 
 class GetLeague(Resource):
 
-    def get(self, country):
-        # country = "England"
-        league = models.League.query.filter_by(country = country).first()
+    def get(self):
+        print("here")
+        country = "England"
+        league = models.League.query.filter_by(country = country).first().league_name
         dict = {'country' : country,
                 'league' : league}
         print(f"returned {league}")
-        print(league)
-        league = json.dumps(dict)
         
-        return league
+        return dict
 
 class PostLeague(Resource):
 
     def post(self, league, country):
+        print("here")
         item = models.League(league_name=league, country=country)
         db.session.add(item)
         db.session.commit()
         posted = {'country' : country,
                 'league' : league}
         print(f"posted '{posted}' to the db")
-        print(posted)
-        posted = json.dumps(posted)
         
         return posted
 
@@ -37,6 +34,7 @@ class PostLeague(Resource):
 class PutLeague(Resource):
 
     def put(self, old_league, new_league):
+        print("here")
         league = models.League.query.filter_by(league_name = old_league).first()
         league.league_name = new_league
         db.session.commit()
@@ -44,7 +42,6 @@ class PutLeague(Resource):
         print(changed)
         changed = {'old_league':old_league,
                     'new_league':new_league}
-        changed = json.dumps(changed)
         
         return changed
 
@@ -52,14 +49,14 @@ class PutLeague(Resource):
 class DeleteLeague(Resource):
 
     def delete(self, id):
+        print("here")
         models.League.query.filter_by(id = id).delete()
         deleted = {'deleted':id}
-        deleted = json.dumps(deleted)
         print(f"deleted record with id: {id}")
         print(deleted)
         return deleted
 
-api.add_resource(GetLeague, '/league/getleague/<string:country>')
+api.add_resource(GetLeague, '/league/getleague/')
 api.add_resource(PostLeague, '/league/postleague/<string:league>&<string:country>')
 api.add_resource(PutLeague, '/league/putleague/<string:old_league>&<string:new_league>')
 api.add_resource(DeleteLeague, '/league/deleteleague/<int:id>')
