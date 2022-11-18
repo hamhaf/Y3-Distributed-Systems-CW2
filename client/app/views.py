4f7ef5d0f16edb717fda696b32b0bce23e8693e8
Hamzah Hafejee <sc19mhh@leeds.ac.uk>
2022-11-18 21:37:15 +0000

    error with calling API in this way

--- client/app/views.py
+++ client/app/views.py
@@ -7,21 +7,31 @@ import os,time
 from datetime import date
 from werkzeug.utils import secure_filename
 import requests
+import http.client
+
 
 @app.route('/')
 def index():#the home page will be the login/sign up page
 	form = CountryForm()
 	return render_template('home.html', title='Country Select', form=form)#renders the home.html template passing the appropriate variabels
 
-@app.route('/findLeague', methods=['GET'])
+@app.route('/findLeague', methods=['POST','GET'])
 def findLeague():
     form = CountryForm()
     country = form.country.data
-    league = requests.get(f"127.0.0.1:8001/league/getleague/{country}")
-    print(league)
-    return redirect(url_for())
+    try:
+        conn = http.client.HTTPSConnection("127.0.0.1:8001")
+        conn.request("GET",f"/league/getleague/{country}")
+        res = conn.getresponse()
+        league = res.read()
+        session['league'] = league
+    except:
+        league = "no response"
+    flash(league)
+    return redirect(url_for('index'))
 
-@app.route('showLeague')
-def showLeague():
-    # teams = list of teams from adams api
-    return render_template('showLeague.html', title='League Name'```, teams = teams```)
+# @app.route('/showLeague')
+# def showLeague():
+#     league = session.get('league')
+#     # teams = list of teams from adams api
+#     return render_template('showLeague.html', title='League Name', league=league)
